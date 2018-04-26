4d7958366288184cdf5e9c8cc4e07056ad182fc3
towavephone <634407147@qq.com>
2018-04-27 00:20:37 +0800

    feat: fix slider bug

--- themes/yelee/source/js/mobile.js
+++ themes/yelee/source/js/mobile.js
@@ -44,21 +44,58 @@ define([], function(){
         var slideout = new Slideout({
             'panel': document.getElementsByClassName('mid-col')[0],
             'menu': document.getElementById('viewer'),
-            'padding': 300,
-            'tolerance': 70
+            'padding': 250,
+            'tolerance': 70,
+            'touch': false
           });
     
         // Toggle button
         document.querySelector('.slider-trigger').addEventListener('click', function() {
-            slideout.toggle();
+            slideout.open();
         });
 
+        function close(eve) {
+            eve.preventDefault();
+            slideout.close();
+        }
+          
+        slideout.on('beforeopen', function() {
+            this.panel.classList.add('mid-col-open');
+            $('.slideout-panel').css('z-index', 999);
+        })
+        .on('open', function() {
+            this.panel.addEventListener('click', close);
+        })
+        .on('beforeclose', function() {
+            this.panel.classList.remove('mid-col-open');
+            this.panel.removeEventListener('click', close);
+            $('.slideout-panel').css('z-index', 3);
+        });
         $(window).resize(function(){
-            debugger
             if(slideout.isOpen()){
                 slideout.close();
             }
         })
+        //滚动样式
+        var $overlay = $("#mobile-nav .overlay");
+        var $header = $(".js-mobile-header");
+        fixHeader();
+        window.onscroll = function(){
+            fixHeader();
+        };
+        function fixHeader(scrollTop){
+            var scrollTop = document.documentElement.scrollTop + document.body.scrollTop;
+            if(scrollTop >= 69){
+                $overlay.addClass("fixed");
+            }else{
+                $overlay.removeClass("fixed");
+            }
+            if(scrollTop >= 160){
+                $header.removeClass("hide").addClass("fixed");
+            }else{
+                $header.addClass("hide").removeClass("fixed");
+            }
+        }
     };
 
 
